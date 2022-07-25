106a027f8f753616af9dee4cbde6daab4c1a974c
Trevor Sears <trevor@trevorsears.com>
2022-07-25 00:07:11 -0400

    Added very beginnings of query objects.

---
+++ ts/query/mysql-query-statement.ts
@@ -0,0 +1,38 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 7:27 PM -- July 22nd, 2022
+ * Project: mysql-toolkit-js
+ */
+
+import { Queryable } from "../connection/queryable";
+import { MySQLQueryResults } from "../util/mysql-query-results";
+import { EscapeFunctions } from "mysql";
+
+export abstract class MySQLQueryStatement {
+	
+	/**
+	 * Builds this query into a plain string, returning the result.
+	 * 
+	 * @returns {string} A plain string representation for the query represented
+	 * by this MySQLQueryBuilder.
+	 */
+	public abstract build(escapingAgent: EscapeFunctions): string;
+	
+	/**
+	 * Executes this statement on the provided {@link Queryable} instance and
+	 * returns the result.
+	 * 
+	 * @param {Queryable} queryable The Queryable instance on which to execute
+	 * this statement.
+	 * @returns {Promise<MySQLQueryResults>} The result of executing this
+	 * statement against the provided Queryable.
+	 */
+	public async execute(queryable: Queryable): Promise<MySQLQueryResults> {
+		
+		return await queryable.query(
+			this.build(queryable)
+		);
+		
+	}
+	
+}
