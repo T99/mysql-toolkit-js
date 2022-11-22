caed81f27d31c2784d6fef6821f8a78580116b7a
Trevor Sears <trevor@trevorsears.com>
2022-11-21 21:33:59 -0500

    Moved 'SELECT' statement portion of query-objects testing into new `ts/tests/query-objects` directory.

--- ts/tests/query-objects/select-statement.test.ts
+++ ts/tests/query-objects/select-statement.test.ts
@@ -14,6 +14,11 @@ let database: DatabaseConnectionPool;
 beforeAll((): void => {
 	
 	database = new DatabaseConnectionPool({
+		host: "",
+		user: "",
+		password: "",
+		database: "",
+		connectionLimit: 10,
 	});
 	
 });
